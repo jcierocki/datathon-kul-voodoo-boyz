6501b2c27ad36c5a8f57833c9913b147478530e2
kristaps.greitans <kristaps.greitans@edi.lv>
2023-02-17 21:33:43 +0200

    Added file for scraping images from the database url

---
+++ image_download.py
@@ -0,0 +1,40 @@
+import pandas
+import pyarrow
+import os
+import requests
+from bs4 import BeautifulSoup
+import re
+from urllib.parse import quote, urlsplit, urlunsplit, unquote
+
+data = pandas.read_parquet("./data/Generated.parquet.gzip")
+print(data.count())  # example of operation on the returned DataFrame
+ca_bundle = "/usr/lib/ssl/cert.pem"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+
+if not os.path.exists('generated_images'):
+    os.mkdir('generated_images')
+
+for index, row in data.iterrows():
+    try:
+        # Split the URL into its components and quote each component
+        scheme, netloc, path, query, fragment = urlsplit(row['url'])
+        path = quote(path)
+        query = quote(query, safe='=&')
+        fragment = quote(fragment)
+        url = urlunsplit((scheme, netloc, path, query, fragment))
+
+        response = requests.get(url, headers=headers, verify=ca_bundle)
+        if response.status_code != 200:
+            print(
+                f'Error downloading image from {url}, response: {response.content}')
+        else:
+            # Get the name of the painting from the URL
+            painting_name = os.path.splitext(
+                unquote(os.path.basename(url)))[0]
+
+            with open(f'generated_images/{painting_name}_{index}.jpg', 'wb') as f:
+                f.write(response.content)
+
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
---
+++ painting_clasifier.py
@@ -0,0 +1,25 @@
+from ultralyticsplus import YOLO, postprocess_classify_output
+import os
+
+# load model
+model = YOLO('keremberke/yolov8m-painting-classification')
+
+# set model parameters
+model.overrides['conf'] = 0.25  # model confidence threshold
+
+# set path to image folder
+image_folder = './generated_images/'
+
+# loop through images in folder
+for filename in os.listdir(image_folder):
+    if filename.endswith('.jpg'):
+        # set image
+        image_path = os.path.join(image_folder, filename)
+
+        # perform inference
+        results = model.predict(image_path)
+
+        # observe results
+        print(f'{filename}: {results[0].probs}') # e.g. myimage.jpg: [0.1, 0.2, 0.3, 0.4]
+        processed_result = postprocess_classify_output(model, result=results[0])
+        print(f'{filename}: {processed_result}') # e.g. myimage.jpg: {"cat": 0.4, "dog": 0.6}
